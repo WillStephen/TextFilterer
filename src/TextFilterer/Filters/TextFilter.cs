abe911f9b9148bff4e7b1bb9a963a2551bef99b2
will.stephen@tylbynatwest.com <will.stephen@tylbynatwest.com>
2024-12-03 19:11:54 +0000

    Add xmldoc comments and use regex to split text by whitespace and by punctuation

--- src/TextFilterer/Filters/TextFilter.cs
+++ src/TextFilterer/Filters/TextFilter.cs
@@ -1,14 +1,30 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using TextFilterer.TextMatchers;
 
 namespace TextFilterer.Filters;
 
-public class TextFilter(IEnumerable<ITextMatcher> textMatchers) : ITextFilter
+/// <inheritdoc/>
+/// <summary>
+/// Create a new instance of the <see cref="TextFilterer"/> class.
+/// </summary>
+/// <param name="textMatchers">A set of matchers used to filter words out.</param>
+public partial class TextFilter(IEnumerable<ITextMatcher> textMatchers) : ITextFilter
 {
     private readonly IReadOnlyCollection<ITextMatcher> textMatchers = textMatchers.ToList();
 
+    /// <summary>
+    /// A cache of words that have already been checked. The key is the word itself,
+    /// and the value is a <see cref="bool"/> representing whether the word should be removed from the result.
+    /// </summary>
     private readonly Dictionary<string, bool> filteredWords = [];
 
+    /// <inheritdoc/>
+    /// <remarks>
+    /// Punctuation marks will be preserved in the result.
+    /// For example, if <paramref name="text"/> is <c>The Cat, in the Hat</c>,
+    /// and the word <c>Cat</c> is filtered out, the result will be <c>The , in the Hat</c>.
+    /// </remarks>
     public string FilterText(string text)
     {
         StringBuilder stringBuilder = new();
@@ -41,10 +57,21 @@ public class TextFilter(IEnumerable<ITextMatcher> textMatchers) : ITextFilter
         return stringBuilder.ToString().TrimEnd();
     }
 
+    /// <summary>
+    /// Split a string by spaces and punctuation, preserving the punctuation marks as individual strings,
+    /// but removing the whitespace parts.
+    /// </summary>
     private static string[] SplitToParts(string text)
     {
-        return text
-            .Split(' ')
+        return NonWordsRegex()
+            .Split(text)
+            .Where(str => !string.IsNullOrWhiteSpace(str))
             .ToArray();
     }
+
+    /// <summary>
+    /// Matches characters that are not part of words - whitespace, punctuation, etc.
+    /// </summary>
+    [GeneratedRegex(pattern: @"([^a-zA-Z0-9])")]
+    private static partial Regex NonWordsRegex();
 }
