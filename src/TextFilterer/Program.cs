4901a15de3519c118040fdc2d5a235d28fbc60c2
will.stephen@tylbynatwest.com <will.stephen@tylbynatwest.com>
2024-12-03 20:19:53 +0000

    Add inputFile command-line option, so the user can choose the file they want to run

--- src/TextFilterer/Program.cs
+++ src/TextFilterer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.CommandLine;
 using TextFilterer.Filters;
 using TextFilterer.Outputters;
 using TextFilterer.Repositories;
@@ -6,21 +7,45 @@ using TextFilterer.TextMatchers;
 
 internal class Program
 {
-    public static async Task Main(string[] args)
+    /// <remarks>To run the program, provide an <c>inputFile</c> arg like this: <br/><br/>
+    /// <c>--inputFile "Assets/Alice in Wonderland.txt"</c></remarks>
+    public static async Task<int> Main(string[] args)
     {
+        Option<string> inputFileOption = new(
+            name: "--inputFile",
+            description: "The file to read and filter.")
+        {
+            IsRequired = true,
+            AllowMultipleArgumentsPerToken = false
+        };
+
+        // TODO add an option to preserve/remove punctuation?
+
+        var rootCommand = new RootCommand(description: "A tool to filter out words from text files.");
+
+        rootCommand.AddOption(inputFileOption);
+        rootCommand.SetHandler(RunProgram, inputFileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static async Task RunProgram(string inputFile)
+    {
+        WriteToConsole.InColour($"Filtering words from '{inputFile}'...", ConsoleColor.DarkGray);
+
         IServiceProvider serviceProvider = GetServiceProvider();
 
-        IFileTextFilter fileTextFilter = serviceProvider.GetRequiredService<IFileTextFilter>();
+        IFileTextFilter fileFilterer = serviceProvider.GetRequiredService<IFileTextFilter>();
 
-        await fileTextFilter.FilterAndOutputAsync("Assets/AliceInWonderland.txt");
+        await fileFilterer.FilterAndOutputAsync(inputFile);
     }
 
     private static ServiceProvider GetServiceProvider()
     {
         ServiceCollection serviceCollection = new();
 
-        serviceCollection.AddTransient<ITextFilter, TextFilter>();
         serviceCollection.AddTransient<ITextFileRepository, TextFileRepository>();
+        serviceCollection.AddTransient<ITextFilter, TextFilter>();
         serviceCollection.AddTransient<IFileTextFilter, FileTextFilter>();
         serviceCollection.AddTransient<IResultOutputter, ConsoleResultOutputter>();
 
